8315f13d814adf8ed09454fd4d67e38357e693c4
franciscobaez <9496@holbertonstudents.com>
2024-08-01 14:51:25 -0400

    Erased an if statement that didn't need to be in the file.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,9 +15,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height((*tree).left);
 	right_height = binary_tree_height((*tree).right);
 
-	if ((*tree).right == NULL && (*tree).left == NULL)
-		return (0);
-
 	if (right_height > left_height)
 		return (right_height + 1);
 	return (left_height + 1);

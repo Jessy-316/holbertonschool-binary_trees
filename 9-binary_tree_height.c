e94fc1458ed9552b49f82ea15cc925fb08f476d7
franciscobaez <9496@holbertonstudents.com>
2024-08-01 14:52:58 -0400

    Added an if statement that needed to be in the file.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  * Return: Height of a binary tree in number.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
-{
+i{
 	size_t left_height, right_height;
 
 	if (tree == NULL)
@@ -14,6 +14,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left_height = binary_tree_height((*tree).left);
 	right_height = binary_tree_height((*tree).right);
+
+	if ((*tree).right == NULL && (*tree).left == NULL)
+		return (0);
 
 	if (right_height > left_height)
 		return (right_height + 1);
